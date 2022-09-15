49d749caf38ae21da6769156fbc8e368d650edf0
JangWoojun <woojun0107@naver.com>
2022-09-15 23:20:02 +0900

    2022/09/15 로그인된 사용자 구분하여 상황에 맞는 Activity로 보내주기 구현

--- app/src/main/java/com/example/mysololife/SplashActivity.kt
+++ app/src/main/java/com/example/mysololife/SplashActivity.kt
@@ -5,16 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.mysololife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this,IntroActivity::class.java))
-            finish()
-        },3000)
+        auth = Firebase.auth
+
+        // 로그인 되어있는 사용자면 바로 main으로 안되어 있으면 intro로
+
+        if(auth.currentUser?.uid == null){
+            Handler().postDelayed({
+                startActivity(Intent(this,IntroActivity::class.java))
+                finish()
+            },3000)
+        }
+        else {
+            Handler().postDelayed({
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            },3000)
+        }
+
+
 
     }
 }
