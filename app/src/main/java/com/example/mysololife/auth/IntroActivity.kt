36b38d0a7de056c8b11a168ae0cecac259bcae63
JangWoojun <woojun0107@naver.com>
2022-09-15 15:26:53 +0900

    2022/09/15 비회원 로그인 완성

--- app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/com/example/mysololife/auth/IntroActivity.kt
@@ -3,14 +3,22 @@ package com.example.mysololife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.mysololife.MainActivity
 import com.example.mysololife.R
 import com.example.mysololife.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private lateinit var binding: ActivityIntroBinding
+private lateinit var auth: FirebaseAuth
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_intro)
 
         binding.loginMoveBtn.setOnClickListener {
@@ -23,7 +31,19 @@ class IntroActivity : AppCompatActivity() {
             startActivity(intent)
         }
         binding.noAccountBtn.setOnClickListener {
-
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this,"로그인에 성공하셨습니다",Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        // 뒤로 가기 해도 회원가입 창 안 뜨게
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(this,"로그인에 실패하셨습니다",Toast.LENGTH_LONG).show()
+                    }
+                }
         }
 
     }
