6e26fb22ed53ee214c12e5deae99097dfea8eec5
JangWoojun <woojun0107@naver.com>
2022-09-15 15:21:38 +0900

    2022/09/15 로그아웃 완성

--- app/src/main/java/com/example/mysololife/MainActivity.kt
+++ app/src/main/java/com/example/mysololife/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mysololife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.mysololife.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+            auth.signOut()
+            Toast.makeText(this,"로그아웃 하셨습니다",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, IntroActivity::class.java)
+            // 뒤로 가기 해도 회원가입 창 안 뜨게
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+
     }
 }
