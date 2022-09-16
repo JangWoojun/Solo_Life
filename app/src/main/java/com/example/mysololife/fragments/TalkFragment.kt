81700221e94cb61848d0998fa3f30339c64b8d10
JangWoojun <woojun0107@naver.com>
2022-09-16 13:52:04 +0900

    2022/09/16 토크 fragment 구현

--- app/src/main/java/com/example/mysololife/fragments/TalkFragment.kt
+++ app/src/main/java/com/example/mysololife/fragments/TalkFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.mysololife.R
+import com.example.mysololife.databinding.FragmentTalkBinding
 
 
 class TalkFragment : Fragment() {
-
+private lateinit var binding: FragmentTalkBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +23,21 @@ class TalkFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_talk, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_talk, container, false)
+
+        binding.tipTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_tipFragment)
+        }
+        binding.StoreTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_storeFragment)
+        }
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_bookmarkFragment)
+        }
+        binding.homeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_homeFragment)
+        }
+        return binding.root
     }
 
 
