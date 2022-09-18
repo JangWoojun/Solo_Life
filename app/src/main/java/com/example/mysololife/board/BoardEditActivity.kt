e928806388ff7c313b04e9c690f899adabd4f5d0
JangWoojun <woojun0107@naver.com>
2022-09-18 17:30:20 +0900

    2022/09/18 수정 기능추가

--- app/src/main/java/com/example/mysololife/board/BoardEditActivity.kt
+++ app/src/main/java/com/example/mysololife/board/BoardEditActivity.kt
@@ -1,17 +1,14 @@
 package com.example.mysololife.board
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Button
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.mysololife.R
-import com.example.mysololife.databinding.ActivityBoardInsideBinding
+import com.example.mysololife.databinding.ActivityBoardEditBinding
+import com.example.mysololife.utils.FBAuth
 import com.example.mysololife.utils.FBRef
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
@@ -21,62 +18,32 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.lang.Exception
 
-class BoardInsideActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityBoardInsideBinding
-
+class BoardEditActivity : AppCompatActivity() {
     private lateinit var key : String
+
+    private lateinit var binding : ActivityBoardEditBinding
+
+    private lateinit var writerUid : String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_inside)
-
-//        val title = intent.getStringExtra("title").toString()
-//        val content = intent.getStringExtra("content").toString()
-//        val time = intent.getStringExtra("time").toString()
-
-//        binding.titleArea.text = title
-//        binding.textArea.text = content
-//        binding.timeArea.text = time
-        binding.boardSetting.setOnClickListener {
-            showDialog()
-        }
-
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_edit)
 
         key = intent.getStringExtra("key").toString()
         getBoardData(key)
         getImageData(key)
+        binding.editBtn.setOnClickListener {
+            editBoardDate(key)
+        }
 
 
     }
-
-    private fun showDialog(){
-        val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null)
-        val mBuilder = AlertDialog.Builder(this)
-            .setView(mDialogView)
-            .setTitle("게시글 수정/삭제")
-
-        val alertDialog = mBuilder.show()
-        alertDialog.findViewById<Button>(R.id.editBtn)?.setOnClickListener {
-            val intent = Intent(this,BoardEditActivity::class.java)
-            intent.putExtra("key",key)
-            startActivity(intent)
-        }
-        alertDialog.findViewById<Button>(R.id.removeBtn)?.setOnClickListener {
-            FBRef.boardRef.child(key).removeValue()
-            Toast.makeText(this,"삭제 완료",Toast.LENGTH_LONG).show()
-            finish()
-        }
-
-    }
-
     private fun getImageData(key:String){
 
         val storageReference = Firebase.storage.reference.child(key)
 
-
-
-        val imageViewFB = binding.getImageArea
+        val imageViewFB = binding.imageArea
 
         storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
             if (task.isSuccessful) {
@@ -89,15 +56,16 @@ class BoardInsideActivity : AppCompatActivity() {
     }
 
     private fun getBoardData(key: String){
+
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 try {
                     val dataModel = dataSnapshot.getValue(BoardModel::class.java)
 
-                    binding.titleArea.text = dataModel!!.title
-                    binding.textArea.text = dataModel!!.content
-                    binding.timeArea.text = dataModel!!.time
+                    binding.titleArea.setText(dataModel?.title)
+                    binding.contentArea.setText(dataModel?.content)
+                    writerUid = dataModel!!.uid
                 }
                 catch (e: Exception){
 
@@ -113,5 +81,14 @@ class BoardInsideActivity : AppCompatActivity() {
             }
         }
         FBRef.boardRef.child(key).addValueEventListener(postListener)
+    }
+    private fun editBoardDate(key: String){
+        FBRef.boardRef
+            .child(key)
+            .setValue(
+                BoardModel(binding.titleArea.text.toString(),binding.contentArea.text.toString(),writerUid,FBAuth.getTime())
+            )
+        Toast.makeText(this,"수정완료",Toast.LENGTH_LONG).show()
+        finish()
     }
 }
