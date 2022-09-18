086a909cf9ee8dcfb2b1fd8c4d0ed34ae66cc3ff
JangWoojun <woojun0107@naver.com>
2022-09-18 16:41:50 +0900

    2022/09/17 파이어베이스에서 이미지 다운로드하여 받아오는 기능 구현

--- app/src/main/java/com/example/mysololife/board/BoardInsideActivity.kt
+++ app/src/main/java/com/example/mysololife/board/BoardInsideActivity.kt
@@ -2,9 +2,18 @@ package com.example.mysololife.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.mysololife.R
 import com.example.mysololife.databinding.ActivityBoardInsideBinding
+import com.example.mysololife.utils.FBRef
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class BoardInsideActivity : AppCompatActivity() {
     private lateinit var binding : ActivityBoardInsideBinding
@@ -12,14 +21,58 @@ class BoardInsideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_board_inside)
 
-        val title = intent.getStringExtra("title").toString()
-        val content = intent.getStringExtra("content").toString()
-        val time = intent.getStringExtra("time").toString()
+//        val title = intent.getStringExtra("title").toString()
+//        val content = intent.getStringExtra("content").toString()
+//        val time = intent.getStringExtra("time").toString()
 
-        binding.titleArea.text = title
-        binding.textArea.text = content
-        binding.timeArea.text = time
+//        binding.titleArea.text = title
+//        binding.textArea.text = content
+//        binding.timeArea.text = time
 
 
+        val key = intent.getStringExtra("key")
+        getBoardData(key.toString())
+        getImageData(key.toString())
+
+
+    }
+
+    private fun getImageData(key:String){
+
+        val storageReference = Firebase.storage.reference.child(key)
+
+
+
+        val imageViewFB = binding.getImageArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageViewFB)
+            }
+        })
+
+    }
+
+    private fun getBoardData(key: String){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val dataModel = dataSnapshot.getValue(BoardModel::class.java)
+
+                binding.titleArea.text = dataModel!!.title
+                binding.textArea.text = dataModel!!.content
+                binding.timeArea.text = dataModel!!.time
+
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("TalkFragment", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.child(key).addValueEventListener(postListener)
     }
 }
