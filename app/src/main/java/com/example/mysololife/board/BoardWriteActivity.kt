089f29a6a0b046fbac1acfe1d6733bfa72ddca70
JangWoojun <woojun0107@naver.com>
2022-09-18 15:03:23 +0900

    2022/09/17 파이어베이스 이미지 업로드 기능 구현

--- app/src/main/java/com/example/mysololife/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/mysololife/board/BoardWriteActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mysololife.board
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -12,11 +14,16 @@ import com.example.mysololife.contentsLIst.BookmarkModel
 import com.example.mysololife.databinding.ActivityBoardWriteBinding
 import com.example.mysololife.utils.FBAuth
 import com.example.mysololife.utils.FBRef
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class BoardWriteActivity : AppCompatActivity() {
     private lateinit var binding : ActivityBoardWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_board_write)
 
@@ -26,11 +33,14 @@ class BoardWriteActivity : AppCompatActivity() {
             val uid = FBAuth.getUid()
             val time = FBAuth.getTime()
 
-            FBRef.boardRef
-                .push()
-                .setValue(BoardModel(title,content,uid,time))
+            val key = FBRef.boardRef.push().key.toString()
 
+
+            FBRef.boardRef
+                .child(key)
+                .setValue(BoardModel(title,content,uid,time))
             Toast.makeText(this,"게시글 생성 완료",Toast.LENGTH_LONG).show()
+            imageUpload(key)
             finish()
         }
 
@@ -39,6 +49,31 @@ class BoardWriteActivity : AppCompatActivity() {
             startActivityForResult(gallery,100)
         }
 
+    }
+
+    private fun imageUpload(key:String){
+
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val mountainsRef = storageRef.child(key)
+
+
+        val imageView = binding.imageArea
+
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
